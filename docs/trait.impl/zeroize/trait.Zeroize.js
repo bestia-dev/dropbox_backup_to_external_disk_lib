cf4bdb12758a83b7afa8af7c9725805ecfa9f388
bestia.dev <info@bestia.dev>
2025-09-29 15:01:53 +0000

    update docs

--- docs/trait.impl/zeroize/trait.Zeroize.js
+++ docs/trait.impl/zeroize/trait.Zeroize.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["dropbox_backup_to_external_disk_lib",[["impl Zeroize for <a class=\"struct\" href=\"dropbox_backup_to_external_disk_lib/dropbox_api_token_with_oauth2_mod/struct.SecretResponseAccessToken.html\" title=\"struct dropbox_backup_to_external_disk_lib::dropbox_api_token_with_oauth2_mod::SecretResponseAccessToken\">SecretResponseAccessToken</a>"],["impl Zeroize for <a class=\"struct\" href=\"dropbox_backup_to_external_disk_lib/dropbox_api_token_with_oauth2_mod/struct.SecretResponseRefreshToken.html\" title=\"struct dropbox_backup_to_external_disk_lib::dropbox_api_token_with_oauth2_mod::SecretResponseRefreshToken\">SecretResponseRefreshToken</a>"]]]]);
+    var implementors = Object.fromEntries([["dropbox_backup_to_external_disk_lib",[["impl Zeroize for <a class=\"struct\" href=\"dropbox_backup_to_external_disk_lib/dropbox_api_token_with_oauth2_mod/struct.SecretResponseAccessToken.html\" title=\"struct dropbox_backup_to_external_disk_lib::dropbox_api_token_with_oauth2_mod::SecretResponseAccessToken\">SecretResponseAccessToken</a>"],["impl Zeroize for <a class=\"struct\" href=\"dropbox_backup_to_external_disk_lib/dropbox_api_token_with_oauth2_mod/struct.SecretResponseRefreshToken.html\" title=\"struct dropbox_backup_to_external_disk_lib::dropbox_api_token_with_oauth2_mod::SecretResponseRefreshToken\">SecretResponseRefreshToken</a>"]]],["dropbox_backup_to_external_disk_lib",[["impl Zeroize for <a class=\"struct\" href=\"dropbox_backup_to_external_disk_lib/dropbox_api_token_with_oauth2_mod/struct.SecretResponseAccessToken.html\" title=\"struct dropbox_backup_to_external_disk_lib::dropbox_api_token_with_oauth2_mod::SecretResponseAccessToken\">SecretResponseAccessToken</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[650]}
+//{"start":57,"fragment_lengths":[650,345]}
